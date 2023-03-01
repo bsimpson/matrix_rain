4724b1cc7ba62e724849f176bf8f74cb34e5be6d
Ben Simpson <thehoagie@gmail.com>
2023-03-01 10:26:44 -0500

    10 columns and rows
    Color and characters now randomly change

--- index.js
+++ index.js
@@ -1,35 +1,70 @@
 const CHARS = `ﾊﾐﾋｰｳｼﾅﾓﾆｻﾜﾂｵﾘｱﾎﾃﾏｹﾒｴｶｷﾑﾕﾗｾﾈｽﾀﾇﾍｦｲｸｺｿﾁﾄﾉﾌﾔﾖﾙﾚﾛﾝ`.split('');
-let times = 0;
+const COLS = 10;
+ROWS = 10;
 
-function render() {
-  const blockEl = document.querySelector('.block');
+function render(col) {
+  const blockEl = document.querySelector(`.block .col-${col}`);
 
   setTimeout(() => {
-    // shift down previous chars
-    document.querySelectorAll('.row').forEach((row) => {
+    blockEl.querySelectorAll('.row').forEach((row) => {
+      // shift down previous chars
       let rowNum = Array.from(row.classList)
       rowNum = rowNum.find(klass => klass.match("row-"))
       rowNum = rowNum.match(/row\-(\d+)/)[1];
       row.classList.replace(`row-${rowNum}`, `row-${parseInt(rowNum) + 1}`);
+
+      // shuffle green
+      let green = Array.from(row.classList)
+      green = green.find(klass => klass.match("green-"))
+      green = green.match(/green\-(\d+)/)[1];
+      row.classList.replace(`green-${green}`, `green-${randomGreen(green)}`);
+
+      // shuffle characters
+      row.innerText = randomChar(row.innerText);
     })
 
-    // create new char
+    // create new char at top
     const rowEl = document.createElement('span');
-    rowEl.classList.add('row', `row-${1 + 1}`);
+    rowEl.classList.add('row', `row-1`);
     rowEl.innerText = randomChar();
-    rowEl.classList.add(`green-${Math.ceil(Math.random() * 4)}`)
+    rowEl.classList.add(`green-${randomGreen()}`);
     blockEl.prepend(rowEl);
 
-    times++;
-
-    if (times < 5) {
-      setTimeout(render, 1000);
+    // stop when we run out of rows
+    // TODO randomize length of tail
+    if (blockEl.querySelectorAll('.row').length < ROWS) {
+      setTimeout(() => { render(col) }, blockEl.dataset.speed);
     }
-  }, 1000)
+  // randomize start of rain
+  }, Math.random() * 100);
 }
 
-function randomChar() {
+function randomChar(char) {
+  if (char && Math.random() > .75) {
+    return char;
+  }
+
   return CHARS[Math.floor(Math.random() * CHARS.length)];
 }
 
-window.onload = render;
+function randomGreen(green) {
+  if (green && (Math.random() > .75)) {
+    return green;
+  }
+
+  return Math.ceil(Math.random() * 4);
+}
+
+window.onload = () => {
+  for(let i = 1; i <= COLS; i++) {
+
+    // randomize column speeds
+    document.querySelectorAll('.block .col').forEach(colEl => {
+      colEl.dataset.speed = Math.ceil(Math.random() * 1000);
+    });
+
+    render(i)
+
+    // TODO repeat rain drop when at bottom
+  }
+}
