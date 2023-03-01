bf46f792cbedcf2f5340491e552b5f31f6f4a065
Ben Simpson <thehoagie@gmail.com>
2023-03-01 12:55:58 -0500

    Make columns and rows dynamically styled
    Clear out columns and reset animation with new speed and length

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const CHARS = `ﾊﾐﾋｰｳｼﾅﾓﾆｻﾜﾂｵﾘｱﾎﾃﾏｹﾒｴｶｷﾑﾕﾗｾﾈｽﾀﾇﾍｦｲｸｺｿﾁﾄﾉﾌﾔﾖﾙﾚﾛﾝ`.split('');
-const COLS = 10;
-ROWS = 10;
+const COLS = 50;
+const ROWS = 50;
 
 function render(col) {
   const blockEl = document.querySelector(`.block .col-${col}`);
@@ -10,13 +10,13 @@ function render(col) {
       // shift down previous chars
       let rowNum = Array.from(row.classList)
       rowNum = rowNum.find(klass => klass.match("row-"))
-      rowNum = rowNum.match(/row\-(\d+)/)[1];
+      rowNum = rowNum.match(/row\-(\d*)/)[1];
       row.classList.replace(`row-${rowNum}`, `row-${parseInt(rowNum) + 1}`);
 
       // shuffle green
       let green = Array.from(row.classList)
       green = green.find(klass => klass.match("green-"))
-      green = green.match(/green\-(\d+)/)[1];
+      green = green.match(/green\-(\d*)/)[1];
       row.classList.replace(`green-${green}`, `green-${randomGreen(green)}`);
 
       // shuffle characters
@@ -33,9 +33,51 @@ function render(col) {
     // stop when we run out of rows
     if (blockEl.querySelectorAll('.row').length < blockEl.dataset.length) {
       setTimeout(() => { render(col) }, blockEl.dataset.speed);
+    } else {
+      setTimeout(() => { clearAndRestart(col, blockEl.dataset.speed) });
     }
   // randomize start of rain
   }, Math.random() * 100);
+}
+
+function clearAndRestart(col) {
+  const blockEl = document.querySelector(`.block .col-${col}`);
+
+  setTimeout(() => {
+    let minRowNum = ROWS;
+
+    blockEl.querySelectorAll('.row').forEach((row) => {
+      // shift down previous chars
+      let rowNum = Array.from(row.classList)
+      rowNum = rowNum.find(klass => klass.match("row-"))
+      rowNum = rowNum.match(/row\-(\d*)/)[1];
+
+      if (parseInt(rowNum) >= ROWS) {
+        blockEl.removeChild(row);
+      } else {
+        row.classList.replace(`row-${rowNum}`, `row-${parseInt(rowNum) + 1}`);
+      }
+    });
+
+    // create new char at top
+    const rowEl = document.createElement('span');
+    rowEl.classList.add('row', `row-1`);
+    // rowEl.innerText = randomChar();
+    rowEl.classList.add(`green-${randomGreen()}`);
+    blockEl.prepend(rowEl);
+
+    if (Array.from(blockEl.querySelectorAll('.row')).map(el => el.innerText).filter(text => text).length) {
+      // still have clearing out to do
+      setTimeout(() => { clearAndRestart(col, blockEl.dataset.speed) });
+    } else {
+      // reset column speed, length, rows, and render again
+      blockEl.innerHTML = null;
+      blockEl.dataset.speed = Math.ceil(Math.random() * 1000);
+      blockEl.dataset.length = Math.ceil(Math.random() * ROWS);
+      setTimeout(() => { render(col) }, blockEl.dataset.speed);
+    }
+
+  }, blockEl.dataset.speed);
 }
 
 function randomChar(char) {
@@ -55,7 +97,24 @@ function randomGreen(green) {
 }
 
 window.onload = () => {
+  // dynamically define styles
+  const inlineStyleEl = document.createElement('style')
+  inlineStyleEl.setAttribute('type', 'text/css');
+  let contents = '';
+  for(let i = 1; i < ROWS; i++) {
+    contents += `.row-${i} { top: ${i - 1}em; }`
+    contents += "\n"
+  }
+  inlineStyleEl.innerText = contents;
+  document.querySelector('body').appendChild(inlineStyleEl);
+
   for(let i = 1; i <= COLS; i++) {
+    // setup columns
+    const blockEl = document.querySelector('.block');
+    const colEl = document.createElement('div');
+    colEl.classList.add('col');
+    colEl.classList.add(`col-${i}`);
+    blockEl.appendChild(colEl);
 
     document.querySelectorAll('.block .col').forEach(colEl => {
       // randomize column speeds
@@ -66,7 +125,5 @@ window.onload = () => {
     });
 
     render(i)
-
-    // TODO repeat rain drop when at bottom
   }
 }
